24825600e046b572a4da408000a69dd2a7b74dcf
ffpangestu <38553365+ffpangestu@users.noreply.github.com>
2025-10-06 09:07:02 +0000

    Deploying to gh-pages from @ ffpangestu/ffpangestu.github.io@8aa858bbf0be15b813ddab5eeeed17a370a83b8d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "about",
+    id: "nav-über-mich",
+    title: "über mich",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
